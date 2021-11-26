acae30f7ba426036ae13ef0762770301ebdb3dc3
Valentin W <vwojtasinski@insitaction.com>
2021-11-26 12:02:12 +0100

    feat: template list patients

--- src/app/pages/clients/index/index.component.ts
+++ src/app/pages/clients/index/index.component.ts
@@ -25,46 +25,13 @@ const fakeDataUrl =
   styleUrls: ['./index.component.scss'],
 })
 export class IndexComponent implements OnInit {
-  initLoading = true;
-  loadingMore = false;
-  data: any[] = [];
-  list: Array<{ loading: boolean; name: any }> = [];
   item$: Observable<Patient[]>;
 
   constructor(private http: HttpClient, firestore: Firestore) {
     const patients = collection(firestore, 'patients');
     //@ts-ignore
     this.item$ = collectionData(patients);
-    this.item$.forEach((item) => console.log(item));
   }
 
-  ngOnInit(): void {
-    this.getData((res: any) => {
-      this.data = res.results;
-      this.list = res.results;
-      this.initLoading = false;
-    });
-  }
-
-  getData(callback: (res: any) => void): void {
-    this.http
-      .get(fakeDataUrl)
-      .pipe(catchError(() => of({ results: [] })))
-      .subscribe((res: any) => callback(res));
-  }
-
-  onLoadMore(): void {
-    this.loadingMore = true;
-    this.list = this.data.concat(
-      [...Array(count)].fill({}).map(() => ({ loading: true, name: {} }))
-    );
-    this.http
-      .get(fakeDataUrl)
-      .pipe(catchError(() => of({ results: [] })))
-      .subscribe((res: any) => {
-        this.data = this.data.concat(res.results);
-        this.list = [...this.data];
-        this.loadingMore = false;
-      });
-  }
+  ngOnInit(): void {}
 }
